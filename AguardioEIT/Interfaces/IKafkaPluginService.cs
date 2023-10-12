64bc808b21e3cf648c45f5f6de63867262b16646
Anton <antje12@student.sdu.dk>
2023-10-12 11:59:30 +0200

    Added Kafka plugin

---
+++ AguardioEIT/Interfaces/IKafkaPluginService.cs
@@ -0,0 +1,10 @@
+﻿namespace Interfaces;
+
+public interface IKafkaPluginService
+{ 
+    string Test(); 
+    string Status(); 
+    string Produce(); 
+    string ConsumeStart(); 
+    string ConsumeStop(); 
+}
---
+++ AguardioEIT/KafkaPlugin/Kafka.cs
@@ -0,0 +1,12 @@
+﻿using Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KafkaPlugin;
+
+public class Kafka : IPlugin
+{
+    public void Initialize(IServiceCollection services)
+    {
+        services.AddSingleton<KafkaService>();
+    }
+}
