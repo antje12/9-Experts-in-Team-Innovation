8ff530ec1eb7507f9986ba9fe47e9081f25e4434
Anton <antje12@student.sdu.dk>
2023-11-23 13:58:30 +0100

    common data types

--- AguardioEIT/Common/Models/LeakSensorDataSimple.cs
+++ AguardioEIT/Common/Models/LeakSensorDataSimple.cs
@@ -5,9 +5,9 @@ public class LeakSensorDataSimple
     public int DataRawId { get; set; }
     public string DCreated { get; set; }
     public string DReported { get; set; }
-    public int DLifeTimeUseCount { get; set; }
+    public string DLifeTimeUseCount { get; set; }
     public int LeakLevelId { get; set; }
     public int SensorId { get; set; }
-    public float DTemperatureOut { get; set; }
-    public float DTemperatureIn { get; set; }
+    public string DTemperatureOut { get; set; }
+    public string DTemperatureIn { get; set; }
 }
